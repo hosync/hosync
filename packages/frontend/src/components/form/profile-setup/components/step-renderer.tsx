be204e291c52da8a2331b313d7b00a8380353dc0
FoggDev <51180821+FoggDev@users.noreply.github.com>
2025-02-08 00:58:42 -0800

    Adding Step 3

--- packages/frontend/src/components/form/profile-setup/components/step-renderer.tsx
+++ packages/frontend/src/components/form/profile-setup/components/step-renderer.tsx
@@ -1,7 +1,7 @@
 import { Step1 } from './step1'
 import { Step2 } from './step2'
+import { Step3 } from './step3'
 
-// import Step3 from './step3'
 // import Step4 from './step4'
 // import Step5 from './step5'
 // import Step6 from './step6'
@@ -14,8 +14,8 @@ const StepRenderer = ({ currentStep }: { currentStep?: number }) => {
       return <Step1 />
     case 2:
       return <Step2 />
-    // case 3:
-    //   return <Step3 />
+    case 3:
+      return <Step3 />
     // case 4:
     //   return <Step4 />
     // case 5:
---
+++ packages/frontend/src/components/form/profile-setup/components/step3.tsx
@@ -0,0 +1,14 @@
+import { useProfileSetupForm } from '@/hooks/forms/useProfileSetupForm'
+
+import CabinSetup from './cabin-setup'
+
+const Step3: React.FC = () => {
+  const { state } = useProfileSetupForm()
+  const { values } = state
+
+  if (values.propertyType === 'cabin') {
+    return <CabinSetup />
+  }
+}
+
+export { Step3 }
