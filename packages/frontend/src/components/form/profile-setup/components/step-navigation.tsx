1dadab8101de320b93342761ee6f2132b2b16bcd
FoggDev <51180821+FoggDev@users.noreply.github.com>
2025-02-09 23:28:28 -0800

    Fixing navigation Next button for no ammenities

--- packages/frontend/src/components/form/profile-setup/components/step-navigation.tsx
+++ packages/frontend/src/components/form/profile-setup/components/step-navigation.tsx
@@ -1,5 +1,6 @@
 import { RenderBlockIf } from '@/components/helpers/render-block-if'
 import { Button } from '@/components/ui/button'
+import { useProfileSetupForm } from '@/hooks/forms/useProfileSetupForm'
 
 import { StepIndicator } from './step-indicator'
 
@@ -12,6 +13,15 @@ const StepNavigation = ({
   nextStep: () => void
   currentStep: number
 }) => {
+  const { state } = useProfileSetupForm()
+  const { values } = state
+
+  const ammenities = Object.values(values.amenities)
+  const hasAmenities =
+    ammenities.filter((amenity) => amenity === true).length > 0
+
+  const disabled = currentStep === 4 && !hasAmenities
+
   return (
     <div className="sticky h-20 mt-3 bg-white dark:bg-black z-50 pt-4 border-t border-gray-100 dark:border-gray-800">
       <div className="flex items-center h-full">
@@ -25,7 +35,12 @@ const StepNavigation = ({
           <StepIndicator steps={8} currentStep={currentStep} />
 
           <RenderBlockIf isTrue={currentStep !== 2}>
-            <Button color="primary" onClick={nextStep} className="h-12">
+            <Button
+              color="primary"
+              onClick={nextStep}
+              className="h-12"
+              disabled={disabled}
+            >
               {currentStep < 6 ? 'Next' : 'Finish'}
             </Button>
           </RenderBlockIf>
